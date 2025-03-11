7ad342a4e8d8b3e92a49228b62379855b18cf369
nguyen van thich <thich0811@gmail.com>
2025-03-11 23:06:01 +0700

    final

--- src/main/java/com/nvt/laptopshop/service/VNPayService.java
+++ src/main/java/com/nvt/laptopshop/service/VNPayService.java
@@ -14,8 +14,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
+import jakarta.crypto.Mac;
+import jakarta.crypto.spec.SecretKeySpec;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
